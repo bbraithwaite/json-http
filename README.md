df9a476ab1f58270b516becea67946ac070dd4c3
bbraithwaite <bradley.braithwaite@gmail.com>
2015-03-11 04:09:05 +0100

    name change

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# simple-http
+# json-http
 
 A simple wrapper for making an http get request to a JSON endpoint in Node.js. Includes the ability to set a timeout.
 
 ``` js
-var simpleHttp = require('simple-http');
+var jsonHttp = require('json-http');
 
-simpleHttp.getJson('http://localhost/api', function(err, response) {
+jsonHttp.getJson('http://localhost/api', function(err, response) {
 	console.log(response);      
 });
 ```
@@ -13,10 +13,10 @@ simpleHttp.getJson('http://localhost/api', function(err, response) {
 ## Using Timeout
 
 ``` js
-var simpleHttp = require('simple-http');
+var jsonHttp = require('simple-http');
 var timeoutInMilliSeconds = 2000;
 
-simpleHttp.getJson('http://localhost/api', timeoutInMilliSeconds, function(err, response) {
+jsonHttp.getJson('http://localhost/api', timeoutInMilliSeconds, function(err, response) {
 	console.log(response);      
 });
 ```
--- test/get-json.test.js
+++ test/get-json.test.js
@@ -4,13 +4,13 @@
  * Module dependencies.
  */
 var should = require('should');
-var simpleHttp = require('../index');
+var jsonHttp = require('../index');
 
 describe('Get', function() {
 
   it('returns response', function(done) {
     var url = 'http://www.omdbapi.com/?t=Terminator&y=&plot=short&r=json';
-    simpleHttp.getJson(url, function(err, response) {
+    jsonHttp.getJson(url, function(err, response) {
       should.exist(response);
       done();
     });
@@ -18,7 +18,7 @@ describe('Get', function() {
 
   it('returns timeout error message', function(done) {
     var url = 'http://www.omdbapi.com/?t=Terminator&y=&plot=short&r=json';
-    simpleHttp.getJson(url, 2, function(err) {
+    jsonHttp.getJson(url, 2, function(err) {
       err.should.equal('timeout exceeded');
       done();
     });
@@ -26,7 +26,7 @@ describe('Get', function() {
 
   it('returns error message', function(done) {
     var url = 'http://localhost';
-    simpleHttp.getJson(url, 2, function(err) {
+    jsonHttp.getJson(url, 2, function(err) {
       err.code.should.eql('ECONNREFUSED');
       done();
     });
